5bbc61d087ee1d3903b07f22a1ef61039dc39a1b
Wendell Oliveira <wendellsilvaoliveira9@gmail.com>
2025-05-03 00:15:44 -0300

    adicionado base do servidor backend e modificado package.json

---
+++ backend/scr/server.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+require("dotenv").config();
+
+const port = process.env.PORT || 9090;
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+
